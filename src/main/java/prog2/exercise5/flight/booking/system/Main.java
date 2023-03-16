8f8ca7d04788d668563944394b6337acd1eab780
MyIdea <2035645950@qq.com>
2023-03-16 09:22:05 +0800

    123

---
+++ src/main/java/prog2/exercise5/flight/booking/system/Main.java
@@ -0,0 +1,121 @@
+package prog2.exercise5.flight.booking.system;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.EnumMap;
+public class Main {
+    public static void main( String[] args )
+    {
+//        FlightBooking ps1 = new FlightBooking();
+//        //读取
+//        Scanner sc = new Scanner(System.in);
+//
+//        //1
+//        ps1.setFlightCompany("Flights-of-Fancy");
+//
+//        System.out.println("Please write your name:");
+//        String passengerFullName = sc.next();
+//        //读取乘客需要的门票总数
+//        System.out.println("Please write how many tickets you want:");
+//        int size = sc.nextInt();
+
+
+        //读取顾客姓名
+
+
+
+
+        //读取乘客的出发地和出发机场
+//        System.out.println("Please choose your TripSource:");
+//        System.out.println("1. Nanjing \n" +
+//                "2. Beijing \n" +
+//                "3. Oulu \n" +
+//                "4. Helsinki\n");
+//        int ts1 = sc.nextInt();
+//        String TripSource1 = Integer.toString(ts1);
+//        ps1.setTripSource(TripSource1);
+//
+//        ps1.setSourceAirport(TripSource1);
+//
+//        //读取乘客的目的地和目的机场
+//        System.out.println("Please choose your TripDestination:");
+//        System.out.println("1. Nanjing \n" +
+//                "2. Beijing \n" +
+//                "3. Oulu \n" +
+//                "4. Helsinki\n");
+//        int sd1 = sc.nextInt();
+//        String TripDestination1 = Integer.toString(sd1);
+//        ps1.setTripDestination(TripSource1, TripDestination1);
+//
+//        ps1.setDestinationAirport(TripDestination1);
+//
+//        //System.out.println("Please write your departureDate:");
+//        //String departureDate = sc.next();
+//
+//        //读取出发时间
+//        SimpleDateFormat sdf1 = new SimpleDateFormat("YYYY-MM-DD");
+//        System.out.println("convert a String to date：" + sdf1.format(new Date()));
+//        Date departureDate = null;
+//        try {
+//            departureDate = sdf1.parse("2023-03-07");
+//        }catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+//
+//        //System.out.println("Please write your returnDate:");
+//        //String returnDate = sc.next();
+//        //读取回程时间
+//        SimpleDateFormat sdf2 = new SimpleDateFormat("YYYY-MM-DD");
+//        System.out.println("convert a String to date：" + sdf2.format(new Date()));
+//        Date returnDate = null;
+//        try {
+//            returnDate = sdf2.parse("2023-03-07");
+//        }catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+//
+//        //读取顾客输入的舱位
+//        System.out.println("print your choice:");
+//        //String seatnumber =  sc.next();
+//        int seat0 = sc.nextInt();
+//        String seatnumber = Integer.toString(seat0);
+//        ps1.setBookingClass(seatnumber);
+//        //读取乘客的航行种类
+//        System.out.println("print your choice:");
+//        //String traveltype =  sc.next();
+//        int type = sc.nextInt();
+//        String traveltype = Integer.toString(type);
+//        ps1.setTripType(traveltype);
+//        ps1.setTicketNumber(traveltype, seatnumber, TripSource1, TripDestination1);
+//
+//        //
+//        System.out.println("the number of childpassengers");
+//        int childPassengers = sc.nextInt();
+//        System.out.println("the number of adultpassengers");
+//        int adultPassengers = sc.nextInt();
+//        int  totalPassengers = childPassengers + adultPassengers;
+//        //读取并计算乘客的票价
+//        System.out.println("Print the number of child");
+//        int child = sc.nextInt();
+//        int adults = sc.nextInt();
+//        ps1.setDepartingTicketPrice(child, adults);
+//        ps1.setReturnTicketPrice();
+//        /*
+//        System.out.println("write your departing seat:");
+//        int seat1 = sc.nextInt();
+//
+//        System.out.println("write your returning seat:");
+//        int seat2 = sc.nextInt();
+//         */
+//        ps1.show1();
+//        System.out.println("passengerFullName:" + passengerFullName);
+//        ps1.show2();
+//        System.out.println("childPassengers" + childPassengers);
+//        System.out.println("adultPassengers:" + adultPassengers);
+//        System.out.println("totalPassengers:" + totalPassengers);
+//        ps1.show3();
+    }
+}
